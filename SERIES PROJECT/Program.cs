31f12ee5cb43c75facc41efc0530d43d4e1894ac
David-396 <david.eini12@gmail.com>
2025-05-12 16:01:12 +0300

    Add project files.

---
+++ SERIES PROJECT/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SERIES_PROJECT
+{
+
+    internal class Program
+    {
+
+        static int[] numbersList = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        static int[] series;
+
+        static void PrintEnterSeriesAgain()
+        {
+            
+        }
+
+        static string[] GetTheSeriesAgain() 
+        {
+            
+        }
+
+        static int[] ConvertStringArrayToIntArray(string[] stringLst)
+        {
+
+        }
+        
+        static void PrintTheMenu()
+        {
+
+        }
+
+        static void GetTheOptionFromMenu()
+        {
+
+        }
+
+        static bool ValidationOfTheSeries(string[] stringLst)
+        {
+
+        }
+
+        static void PrintWrongSeries()
+        {
+
+        }
+
+        static bool ValidationOfTheOption()
+        {
+
+        }
+
+        static void PrintWrongOption()
+        {
+
+        }
+
+
+        static void ReplaceTheSeriesManager_opt1()
+        {
+
+        }
+
+        static void ReplaceSeries_opt1()
+        {
+
+        }
+
+
+        static void PrintTheSeriesInOriginalOrderManager_opt2()
+        {
+
+        }
+
+        static void PrintTheSeriesInReverseManager_opt3()
+        {
+
+        }
+
+        static void PrintTheSeriesInAscendingOrderManager_opt4()
+        {
+        }
+
+        static void PrintTheMaxNumInSeriesManager_opt5()
+        {
+
+        }
+
+        static void PrintTheMinNumInSeriesManager_opt6()
+        {
+        }
+
+        static void PrintTheAverageOfTheSeriesManager_opt7()
+        {
+
+        }
+
+        static void printTheLengthOfTheSeriesManager_opt8()
+        {
+
+        }
+
+        static void PrintTheSumOfTheSeriesManager_opt9()
+        {
+
+        }
+
+        static bool PrintExitMessageManager_opt10()
+        {
+
+        }
+
+
+
+
+        static void ProgramManager()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
